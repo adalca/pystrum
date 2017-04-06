9610dec9334fa59eabc60cd6600328033fa7855d
Adrian Dalca <adalca@mit.edu>
2017-04-06 09:49:53 -0400

    adding a slew of tools to patchlib

--- pytools/patchlib.py
+++ pytools/patchlib.py
@@ -1,7 +1,7 @@
 """
 patchlib (python version)
 
-A powerful library for working with N-D patches.
+A powerful library for working with N-D patches. 
 Modelled after the MATLAB patchlib (https://github.com/adalca/patchlib)
 """
 
@@ -9,6 +9,8 @@ import types
 import numpy as np
 
 import pynd.ndutils as nd
+from imp import reload
+reload(nd)
 
 
 def patch_gen(vol, patch_size, stride=1, nargout=1):
@@ -43,6 +45,104 @@ def patch_gen(vol, patch_size, stride=1, nargout=1):
             yield (vol[patch_sub], patch_sub)
 
 
+def quilt(patches, patch_size, grid_size, patch_stride=1, nan_func=lambda x:np.nanmean(x, 0)):
+    """ patches should be a nPatches-by-nVoxels array, or a patch generator """
+
+    patch_stack = stack(patches, patch_size, grid_size, patch_stride)
+    return nan_func(patch_stack)
+
+
+def stack(patches, patch_size, grid_size, patch_stride=1, nargout=1):
+    """ UNFINISHED """
+
+    assert np.all(np.mod(patch_size, 2) == 1), "patch size is not odd"
+    K = patches.shape[2] if len(patches.shape) > 2 else 1
+
+    # compute the input target_size and target
+    if np.prod(grid_size) == patches.shape[0]: # given the number of patches in the grid
+        target_size = grid2volsize(grid_size, patch_size, patch_stride=patch_stride)
+    else:
+        target_size = grid_size
+
+    # compute the grid indexes (and check that the target size matches)
+    [grid_idx, target_size_chk] = grid(target_size, patch_size, patch_stride, nargout=2)
+    assert np.all(target_size == target_size_chk), 'Target does not match the provided target size'
+
+    # prepare subscript and index vectors
+    grid_sub = nd.ind2sub_entries(grid_idx, target_size)
+    all_idx = list(range(grid_idx.size))
+
+    # get index of layer location so that patches don't overlap
+    # we do this by computing the modulo of the patch start location
+    # with respect to the patch size. This won't be optimal yet, but we'll
+    # eliminate any layers with no patches after
+    mod_sub = np.array([_mod_base(g, patch_size) for g in grid_sub]).transpose()
+    patch_payer_idx = nd.sub2ind(mod_sub, patch_size)
+
+    # initiate the votes layer structure
+    layer_ids = np.unique(patch_payer_idx)
+    nb_layers = len(layer_ids)
+    layers = np.empty([nb_layers, *target_size, K])
+    layers[:] = np.NAN
+
+    # prepare input matching matrix
+    if nargout >= 2:
+        idxmat = np.empty([2, nb_layers, *target_size, K])
+        idxmat[:] = np.NAN
+
+    #  go over each layer index
+    for layer_idx in range(nb_layers):
+        # get patches attributed to this layer
+        patch_id_in_layer = np.where(patch_payer_idx == layer_ids[layer_idx])
+
+        # prepare the layers
+        layer_stack = np.empty([*target_size, K])
+        layer_stack[:] = np.NAN
+        if nargout >= 2:
+            layer_idxmat = np.nan([2, *target_size, K])
+
+        # go thorugh each patch location for patches in this layer
+        for pidx in patch_id_in_layer[0]:
+
+            # extract the patches
+            localpatches = np.squeeze(patches[pidx, :])
+            patch = np.reshape(localpatches, [*patch_size, K])
+
+            # put the patches in the layers
+            sub = [*grid_sub[pidx, :], 0]
+            endsub = np.array(sub) + np.array([*patch_size, K])
+            rge = nd.range(sub, endsub)
+            layer_stack[rge] = patch
+
+            # update input matching matrix
+            if nargout >= 2:
+                # the linear index of the patch in the grid
+                locidx = np.ones([2, *patch_size, K]) * all_idx[pidx]
+                locidx[1, :] = np.matlib.repmat(list(range(np.prod(patch_size))), 1, K)
+                layer_idxmat[rge] = locidx
+
+        # update layer
+        layers[layer_idx, :] = np.reshape(layer_stack.flatten(), [*target_size, K])
+
+        # update the complete idxmat
+        if nargout >= 2:
+            idxmat[0, layer_idx, :] = layer_idxmat[0, :]
+            idxmat[1, layer_idx, :] = layer_idxmat[1, :]
+
+    # setup outputs
+    if nargout == 1:
+        return layers
+    elif nargout == 2:
+        print("UNCHECKED")
+        return (layers, idxmat)
+    elif nargout == 3:
+        print("UNCHECKED")
+        p = np.zeros(1, np.max(patch_payer_idx.flatten()))
+        p[layer_ids] = list(range(len(layer_ids)))
+        return (layers, idxmat, p[patch_payer_idx])
+
+
+
 
 def grid2volsize(grid_size, patch_size, patch_stride=1):
     """
@@ -63,6 +163,15 @@ def grid2volsize(grid_size, patch_size, patch_stride=1):
         {adalca,klbouman}@csail.mit.edu
     """
 
+    # parameter checking
+    if not isinstance(grid_size, np.ndarray):
+        grid_size = np.array(grid_size, 'int')
+    if not isinstance(patch_size, np.ndarray):
+        patch_size = np.array(patch_size, 'int')
+    nb_dims = len(patch_size)   # number of dimensions
+    if isinstance(patch_stride, int):
+        patch_stride = np.repeat(patch_stride, nb_dims).astype('int')
+
     patch_overlap = patch_size - patch_stride
     vol_size = grid_size * patch_stride + patch_overlap
     return vol_size
@@ -70,9 +179,9 @@ def grid2volsize(grid_size, patch_size, patch_stride=1):
 
 def gridsize(vol_size, patch_size, patch_stride=1, start_sub=0, nargout=1):
     """
-    Number of patches that fit into volSize given a particular patchSize. The volume will be
-    cropped to the maximum size that fits the patch grid. For example, a [6x6] volume with a
-    patchsize of [3x3] and overlap of 1 will be cropped to [5x5] volume.
+    Number of patches that fit into volSize given a particular patch_size. patch_strideb
+    cropped to the maximum size that fits the patch grid. For example, a [6x6] volume with
+    patch_size opatch_stridee
 
     Parameters:
         vol_size (numpy vector): the size of the input volume
@@ -97,6 +206,17 @@ def gridsize(vol_size, patch_size, patch_stride=1, start_sub=0, nargout=1):
         {adalca,klbouman}@csail.mit.edu
     """
 
+    # parameter checking
+    if not isinstance(vol_size, np.ndarray):
+        vol_size = np.array(vol_size, 'int')
+    if not isinstance(patch_size, np.ndarray):
+        patch_size = np.array(patch_size, 'int')
+    nb_dims = len(patch_size)   # number of dimensions
+    if isinstance(patch_stride, int):
+        patch_stride = np.repeat(patch_stride, nb_dims).astype('int')
+    if isinstance(start_sub, int):
+        start_sub = np.repeat(start_sub, nb_dims).astype('int')
+
     # adjacent patch overlap
     patch_overlap = patch_size - patch_stride
 
@@ -120,7 +240,7 @@ def gridsize(vol_size, patch_size, patch_stride=1, start_sub=0, nargout=1):
 
 def grid(vol_size, patch_size, patch_stride=1, start_sub=0, nargout=1, grid_type='idx'):
     """
-    grid of patch starting points for nd volume that fit into that volume
+    grid of patch starting points for nd volume that fit into given volume size
 
     The index is in the given volume. If the volume gets cropped as part of the function and you
     want a linear indexing into the new volume size, use
@@ -203,3 +323,20 @@ def grid(vol_size, patch_size, patch_stride=1, start_sub=0, nargout=1, grid_type
 def _row_gen(nparray):
     for rowi in nparray.shape[0]:
         yield nparray[rowi, :]
+
+def _mod_base(X, Y, base=0):
+    """
+    modulo with respect to a specific base numbering system
+    i.e. returns base + ((X - base) % Y)
+    x = modBase(X, Y) behaves like X % Y
+
+    Parameters:
+        X (array_like): divident
+        Y (array_like): divisor
+        base (optional, default 0): the base
+
+    Returns:
+        the modulo
+    """
+
+    return base + np.mod(X - base, Y)
