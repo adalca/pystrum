584426d6cd02dded6baa194cd2fd229c4dcc4610
Adrian Dalca <adalca@sumac.csail.mit.edu>
2017-10-18 09:07:56 -0400

    colors bug fix

--- pynd/segutils.py
+++ pynd/segutils.py
@@ -7,7 +7,7 @@ Contact: adalca@csail.mit.edu
 import numpy as np
 from . import ndutils as nd
 
-def seg2contour(seg, exclude_zero=True, contour_type='inner'):
+def seg2contour(seg, exclude_zero=True, contour_type='inner', thickness=1):
     '''
     transform nd segmentation (label maps) to contour maps
 
@@ -44,7 +44,8 @@ def seg2contour(seg, exclude_zero=True, contour_type='inner'):
         label_map = seg == lab
 
         # extract contour map for this label
-        label_contour_map = nd.bw2contour(label_map, type=contour_type)
+        thickness = thickness + 0.01
+        label_contour_map = nd.bw2contour(label_map, type=contour_type, thr=thickness)
 
         # assign contour to this label
         contour_map[label_contour_map] = lab
@@ -53,16 +54,20 @@ def seg2contour(seg, exclude_zero=True, contour_type='inner'):
 
 
 
-def seg_overlap(vol, seg, do_contour=True, do_rgb=True, cmap=None):
+def seg_overlap(vol, seg, do_contour=True, do_rgb=True, cmap=None, thickness=1.0):
     '''
     overlap a nd volume and nd segmentation (label map)
+
+    do_contour should be None, boolean, or contour_type from seg2contour
 
     not well tested yet.
     '''
 
     # compute contours for each label if necessary
-    if do_contour:
-        seg = seg2contour(seg)
+    if do_contour is not None and do_contour is not False:
+        if not isinstance(do_contour, str):
+            do_contour = 'inner'
+        seg = seg2contour(seg, contour_type=do_contour, thickness=thickness)
 
     # compute a rgb-contour map
     if do_rgb:
@@ -71,7 +76,7 @@ def seg_overlap(vol, seg, do_contour=True, do_rgb=True, cmap=None):
             colors = np.random.random((nb_labels, 3)) * 0.5 + 0.5
             colors[0, :] = [0, 0, 0]
         else:
-            colors = cmap
+            colors = cmap[:, 0:3]
 
         olap = colors[seg.flat, :]
         sf = seg.flat == 0
@@ -82,6 +87,36 @@ def seg_overlap(vol, seg, do_contour=True, do_rgb=True, cmap=None):
     else:
         olap = seg
         olap[seg == 0] = vol[seg == 0]
+
+    return olap
+
+
+def seg_overlay(vol, seg, do_rgb=True, seg_wt=0.5, cmap=None):
+    '''
+    overlap a nd volume and nd segmentation (label map)
+
+    not well tested yet.
+    '''
+
+    # compute contours for each label if necessary
+
+    # compute a rgb-contour map
+    if do_rgb:
+        if cmap is None:
+            nb_labels = np.max(seg) + 1
+            colors = np.random.random((nb_labels, 3)) * 0.5 + 0.5
+            colors[0, :] = [0, 0, 0]
+        else:
+            colors = cmap[:, 0:3]
+
+        seg_flat = colors[seg.flat, :]
+        seg_rgb = np.reshape(seg_flat, vol.shape + (3, ))
+
+        # get the overlap image
+        olap = seg_rgb * seg_wt + np.expand_dims(vol, -1) * (1-seg_wt)
+
+    else:
+        olap = seg * seg_wt + vol * (1-seg_wt)
 
     return olap
 
