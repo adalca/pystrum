99748c68e19b2bf53059080998629edf409d3005
Andrew Hoopes <athoopes1@gmail.com>
2020-09-15 21:41:44 -0400

    increase version and change license

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name='pystrum',
-    version='0.1',
-    license='gpl-3.0',
+    version='0.2',
+    license='MIT',
     description='General Python Utility Library',
     url='https://github.com/adalca/pystrum',
     packages=setuptools.find_packages(),
@@ -11,7 +11,7 @@ setuptools.setup(
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     install_requires=[
