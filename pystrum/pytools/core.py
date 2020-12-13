d25cbf26722cd1731decae5adb61d152084d91d7
adalca <adalca@DESKTOP-HOME16.MIT.EDU>
2020-12-13 13:13:23 -0500

    adding humanize filesize

--- pystrum/pytools/core.py
+++ pystrum/pytools/core.py
@@ -1,4 +1,4 @@
-import collections
+import collections, math
 
 def flatten_collection(l):
     # https://stackoverflow.com/questions/2158395/flatten-an-irregular-list-of-lists
@@ -7,3 +7,17 @@ def flatten_collection(l):
             yield from flatten_collection(el)
         else:
             yield el
+
+def humanize_filesize(filesize):
+    """ show human readable file size
+
+    Args:
+        filesize ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    sizes_list = ['B','KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB']
+
+    power = int(math.floor(math.log(filesize, 2)/10))
+    return "%.3f%s" % (filesize/math.pow(1024, power), sizes_list[power])
