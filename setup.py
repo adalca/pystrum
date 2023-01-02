6422922af2607bf740a78526c6be83c388b25c1c
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2023-01-02 11:23:26 -0500

    Update use of Packaging library. (#11)

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 import re
 import pathlib
 import setuptools
+
+setuptools.dist.Distribution().fetch_build_eggs(['packaging'])
 import packaging.version
 
 
@@ -17,7 +19,9 @@ match = re.search(pattern, init_text, re.M)
 if not match:
     raise RuntimeError(f'Unable to find __version__ in {init_file}.')
 version = match.group(1)
-if isinstance(packaging.version.parse(version), packaging.version.LegacyVersion):
+try:
+    packaging.version.parse(version)
+except packaging.version.InvalidVersion:
     raise RuntimeError(f'Invalid version string {version}.')
 
 # run setup
