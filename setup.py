d7b1f456761e15c85778bc6cc0549a93412025ca
ahoopes <hoopes@mit.edu>
2023-06-27 01:32:41 -0400

    no need for fetch_build_eggs in setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,14 @@ import re
 import pathlib
 import setuptools
 
-setuptools.dist.Distribution().fetch_build_eggs(['packaging'])
-import packaging.version
-
-
-# base source directory
-base_dir = pathlib.Path(__file__).parent.resolve()
-
 # extract the current version
-init_file = base_dir.joinpath('pystrum/__init__.py')
+init_file = pathlib.Path(__file__).parent.resolve().joinpath('pystrum/__init__.py')
 init_text = open(init_file, 'rt').read()
 pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
 match = re.search(pattern, init_text, re.M)
 if not match:
-    raise RuntimeError(f'Unable to find __version__ in {init_file}.')
+    raise RuntimeError(f'Unable to find __version__ in {init_file}')
 version = match.group(1)
-try:
-    packaging.version.parse(version)
-except packaging.version.InvalidVersion:
-    raise RuntimeError(f'Invalid version string {version}.')
 
 # run setup
 setuptools.setup(
